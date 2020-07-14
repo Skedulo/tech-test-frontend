a3037acbd81097f64915e6e68b9ec30bc6aebb06
Aaron Abrook <aabrook@skedulo.com>
2020-07-14 16:05:30 +1000

    Update Create React App and convert to functional components (#6)
    Co-authored-by: Drew Taylor <dtaylor@skedulo.com>

--- README.md
+++ README.md
@@ -7,17 +7,17 @@
 
 ## Installation Instructions
 Install:
-    ```npm run bootstrap```
+    ```yarn bootstrap```
 
 This installs everything for the App as well as the Server, so please don't run ```npm install``` (unless you also want to run it from the `server` folder)
 
 ## Running the App
 Run:
-    ```npm run start```
+    ```yarn start```
 
 ## Testing the App
 Run:
-    ```npm run test```
+    ```yarn test```
 
 
 ## Instructions
--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
+import './App.css'
 
 import { DataService } from './service/DataService'
 import { QuestionOne } from './question-one/QuestionOne'
 import { QuestionTwo } from './question-two/QuestionTwo'
 import { QuestionThree } from './question-three/QuestionThree'
-
-import './App.css'
 
 const AppTabs = {
   First: 'first',
@@ -13,63 +12,55 @@ const AppTabs = {
   Third: 'three'
 }
 
-class App extends Component {
-  constructor (props) {
-    super(props)
+const AvailableTabs = ({ onSelect, selectedTab }) => {
+  return (
+    <div className="app__tab-group">
+      <div className={ 'app__tab ' + (selectedTab === AppTabs.First ? 'app__tab--selected' : '') } onClick={ () => onSelect(AppTabs.First) }>First Question</div>
+      <div className={ 'app__tab ' + (selectedTab === AppTabs.Second ? 'app__tab--selected' : '') } onClick={ () => onSelect(AppTabs.Second) }>Second Question</div>
+      <div className={ 'app__tab ' + (selectedTab === AppTabs.Third ? 'app__tab--selected' : '') } onClick={ () => onSelect(AppTabs.Third) }>Third Question</div>
+    </div>
+  )
+}
 
-    const defaultTab = localStorage.getItem('selectedTab') || AppTabs.First
-    this.state = {
-      selectedTab: defaultTab
-    }
-  }
-
-  setSelectedTab = (selectedTab) => {
-    this.setState({
-      selectedTab
-    }, () => localStorage.setItem('selectedTab', this.state.selectedTab))
-  }
-  
-  renderTabs = () => {
-    return (
-      <div className="app__tab-group">
-        <div className={ 'app__tab ' + (this.state.selectedTab === AppTabs.First ? 'app__tab--selected' : '') } onClick={ () => this.setSelectedTab(AppTabs.First) }>First Question</div>
-        <div className={ 'app__tab ' + (this.state.selectedTab === AppTabs.Second ? 'app__tab--selected' : '') } onClick={ () => this.setSelectedTab(AppTabs.Second) }>Second Question</div>
-        <div className={ 'app__tab ' + (this.state.selectedTab === AppTabs.Third ? 'app__tab--selected' : '') } onClick={ () => this.setSelectedTab(AppTabs.Third) }>Third Question</div>
-      </div>
-    )
-  }
-
-  renderContent = () => {
-    switch (this.state.selectedTab) {
-      case AppTabs.Third:
-        return (
-          <QuestionThree service={ DataService } />
-        )
-      case AppTabs.Second:
-        return (
-          <QuestionTwo service={ DataService } />
-        )
-      case AppTabs.First:
-      default:
-        return (
-          <QuestionOne service={ DataService } />
-        )
-    }
-  }
-
-  render() {
-    return (
-      <div className="app__container">
-        <header className="app__header">
-          <h1 className="app__title">Skedulo Technical Test</h1>
-          { this.renderTabs() }
-        </header>
-        <div className={ this.state.selectedTab !== AppTabs.Third ? 'app__content' : 'app__content-blank' }>
-          { this.renderContent() }
-        </div>
-      </div>
-    )
+const Tabs = ({ selectedTab }) => {
+  switch (selectedTab) {
+    case AppTabs.Third:
+      return (
+        <QuestionThree service={ DataService } />
+      )
+    case AppTabs.Second:
+      return (
+        <QuestionTwo service={ DataService } />
+      )
+    case AppTabs.First:
+    default:
+      return (
+        <QuestionOne service={ DataService } />
+      )
   }
 }
 
-export default App;
+function App() {
+  const defaultTab = localStorage.getItem('selectedTab') || AppTabs.First
+
+  const [selectedTab, setSelectedTab] = useState(defaultTab)
+
+  const selectTab = (tab) => {
+    setSelectedTab(tab)
+    localStorage.setItem('selectedTab', tab)
+  }
+
+  return (
+    <div className="app__container">
+      <header className="app__header">
+        <h1 className="app__title">Skedulo Technical Test</h1>
+        <AvailableTabs onSelect={selectTab} selectedTab={selectedTab} />
+      </header>
+      <div className={ selectedTab !== AppTabs.Third ? 'app__content' : 'app__content-blank' }>
+        <Tabs selectedTab={selectedTab} />
+      </div>
+    </div>
+  )
+}
+
+export default App
