6c6a7914e176a44cf6a10fc4f5fa5ceb32ec13a3
soniah7 <zongyahou@gmail.com>
2020-08-25 02:06:36 +1000

    q2 is working but functions may need refactored

--- src/question-two/QuestionTwo.js
+++ src/question-two/QuestionTwo.js
@@ -1,22 +1,105 @@
-import React from 'react';
+import React from "react";
 
-import { SectionGroup } from '../components/section/SectionGroup'
-import { SectionPanel } from '../components/section/SectionPanel'
+import { SectionGroup } from "../components/section/SectionGroup";
+import { SectionPanel } from "../components/section/SectionPanel";
 
-import './QuestionTwo.css';
+import { Swimlane } from "../components/swimlane/Swimlane";
+
+import { DataService } from "../service/DataService";
+import "./QuestionTwo.css";
 
 /**
  * Please do not change these dates, the data on the server all fall within the 01/09/2018
  */
-const RANGE_START = new Date('2018-09-01T00:00:00Z')
-const RANGE_END = new Date('2018-09-01T24:00:00Z')
+const RANGE_START = new Date("2018-09-01T00:00:00Z");
+const RANGE_END = new Date("2018-09-01T24:00:00Z");
 
-export const QuestionTwo = (props) => {
-  return (
-    <SectionGroup>
-      <SectionPanel>
-        Please refer to INSTRUCTIONS.md
-      </SectionPanel>
-    </SectionGroup>
-  )
+export class QuestionTwo extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      lanes: [],
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      this.jobs = await DataService.getJobs();
+      this.activities = await DataService.getActivities();
+      this.resources = await DataService.getResources();
+      this.jobAllocations = await DataService.getJobAllocations();
+      this.activityAllocations = await DataService.getActivityAllocations();
+    } catch (e) {
+      alert("failed to fetch data from axois client");
+    }
+    this.integrateJobsIntoResources();
+    this.integrateActivitiesIntoResources();
+    this.generateLanes();
+  }
+
+  generateLanes = () => {
+    const lanes = this.resources.map((resource) => {
+      const cards = [];
+      for (const jobInfo of resource.jobsInfo) {
+        cards.push({
+          start: new Date(jobInfo.start),
+          end: new Date(jobInfo.end),
+          description: jobInfo.name,
+          style: { color: "red" },
+        });
+      }
+      for (const activityInfo of resource.activitiesInfo) {
+        cards.push({
+          start: new Date(activityInfo.start),
+          end: new Date(activityInfo.end),
+          description: activityInfo.name,
+          style: { color: "blue" },
+        });
+      }
+      return { title: resource.name, cards };
+    });
+    this.setState({ lanes });
+  };
+
+  integrateJobsIntoResources() {
+    const { resources, jobs } = this;
+    resources.forEach((resource) => {
+      resource.jobsInfo = [];
+      const resourceId = resource.id;
+      for (const jobAllocation of this.jobAllocations) {
+        if (Number(jobAllocation.resourceId) === Number(resourceId)) {
+          resource.jobsInfo.push(jobs[jobAllocation.jobId]);
+        }
+      }
+    });
+  }
+
+  integrateActivitiesIntoResources() {
+    const { resources, activities } = this;
+    resources.forEach((resource) => {
+      resource.activitiesInfo = [];
+      const resourceId = resource.id;
+      for (const activityAllocation of this.activityAllocations) {
+        if (Number(activityAllocation.resourceId) === Number(resourceId)) {
+          resource.activitiesInfo.push(
+            activities[activityAllocation.activityId]
+          );
+        }
+      }
+    });
+  }
+
+  render() {
+    return (
+      <SectionGroup>
+        <SectionPanel>
+          <Swimlane
+            lanes={this.state.lanes}
+            start={RANGE_START}
+            end={RANGE_END}
+          />
+        </SectionPanel>
+      </SectionGroup>
+    );
+  }
 }
