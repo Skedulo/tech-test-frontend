f9d11a19480567a8decd5619fbf0dab0a3e35bb9
Brandon Winch <bwinch@skedulo.com>
2021-03-05 13:43:45 +1000

    Patch question two to expect correct data

--- src/question-two/QuestionTwo.test.js
+++ src/question-two/QuestionTwo.test.js
@@ -15,5 +15,55 @@ const mockDataService = {
 test('fetched and merged data is in the expected shape', async () => {
   render(<QuestionTwo service={mockDataService} />)
 
-  expect(await screen.findByText('[{"resourceName":"Sam Seaborn","resourceId":0,"allocations":[{"allocId":0,"allocType":"activity","name":"Meal Break","start":"2018-09-01T12:15:00Z","end":"2018-09-01T13:10:00Z"},{"allocId":1,"allocType":"activity","name":"Meal Break","start":"2018-09-01T12:15:00Z","end":"2018-09-01T13:10:00Z"},{"allocId":0,"allocType":"job","name":"Build a shed","start":"2018-09-01T10:15:00Z","end":"2018-09-01T11:00:00Z"},{"allocId":1,"allocType":"job","name":"Shield some wiring","start":"2018-09-01T09:00:00Z","end":"2018-09-01T13:00:00Z"}]},{"resourceName":"Donna Moss","resourceId":1,"allocations":[{"allocId":0,"allocType":"activity","name":"Meal Break","start":"2018-09-01T12:15:00Z","end":"2018-09-01T13:10:00Z"},{"allocId":1,"allocType":"activity","name":"Meal Break","start":"2018-09-01T12:15:00Z","end":"2018-09-01T13:10:00Z"},{"allocId":0,"allocType":"job","name":"Build a shed","start":"2018-09-01T10:15:00Z","end":"2018-09-01T11:00:00Z"},{"allocId":1,"allocType":"job","name":"Shield some wiring","start":"2018-09-01T09:00:00Z","end":"2018-09-01T13:00:00Z"}]},{"resourceName":"Toby Ziegler","resourceId":2,"allocations":[{"allocId":0,"allocType":"activity","name":"Meal Break","start":"2018-09-01T12:15:00Z","end":"2018-09-01T13:10:00Z"},{"allocId":1,"allocType":"activity","name":"Meal Break","start":"2018-09-01T12:15:00Z","end":"2018-09-01T13:10:00Z"},{"allocId":0,"allocType":"job","name":"Build a shed","start":"2018-09-01T10:15:00Z","end":"2018-09-01T11:00:00Z"},{"allocId":1,"allocType":"job","name":"Shield some wiring","start":"2018-09-01T09:00:00Z","end":"2018-09-01T13:00:00Z"}]}]'))
+  expect(await screen.findByText('[{"resourceName":"Sam Seaborn","resourceId":0,"allocations":[{"allocType":"job","name":"Shield some wiring","start":"2018-09-01T09:00:00Z","end":"2018-09-01T13:00:00Z"}]},{"resourceName":"Donna Moss","resourceId":1,"allocations":[{"allocType":"activity","name":"Meal Break","start":"2018-09-01T12:15:00Z","end":"2018-09-01T13:10:00Z"},{"allocType":"job","name":"Build a shed","start":"2018-09-01T10:15:00Z","end":"2018-09-01T11:00:00Z"}]},{"resourceName":"Toby Ziegler","resourceId":2,"allocations":[{"allocType":"activity","name":"Meal Break","start":"2018-09-01T12:15:00Z","end":"2018-09-01T13:10:00Z"}]}]'))
 })
+
+/**
+ * Expected output:
+
+[
+   {
+      "resourceName":"Sam Seaborn",
+      "resourceId":0,
+      "allocations":[
+         {
+            "allocType":"job",
+            "name":"Shield some wiring",
+            "start":"2018-09-01T09:00:00Z",
+            "end":"2018-09-01T13:00:00Z"
+         }
+      ]
+   },
+   {
+      "resourceName":"Donna Moss",
+      "resourceId":1,
+      "allocations":[
+         {
+            "allocType":"activity",
+            "name":"Meal Break",
+            "start":"2018-09-01T12:15:00Z",
+            "end":"2018-09-01T13:10:00Z"
+         },
+         {
+            "allocType":"job",
+            "name":"Build a shed",
+            "start":"2018-09-01T10:15:00Z",
+            "end":"2018-09-01T11:00:00Z"
+         }
+      ]
+   },
+   {
+      "resourceName":"Toby Ziegler",
+      "resourceId":2,
+      "allocations":[
+         {
+            "allocType":"activity",
+            "name":"Meal Break",
+            "start":"2018-09-01T12:15:00Z",
+            "end":"2018-09-01T13:10:00Z"
+         }
+      ]
+   }
+]
+
+ */
